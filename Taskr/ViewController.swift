51af33d352d4e8885d26d93be7409455004460a3
Maxwell Serota, Jack Shapiro, Ethan Blum, Justin Minerva <s015049@students.lmsd.org>
2020-02-19 07:39:00 -0500

    Initial Commit

---
+++ Taskr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Taskr
+//
+//  Created by Maxwell Serota (student LM) on 2/19/20.
+//  Copyright © 2020 BigBrainCorporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
