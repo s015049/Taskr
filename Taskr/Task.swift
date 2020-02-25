8f31612073bd5b29d54e6609c287cc575458d54f
Maxwell Serota, Jack Shapiro, Ethan Blum, Justin Minerva <s015049@students.lmsd.org>
2020-02-25 08:24:17 -0500

    Replaced the task class.

--- Taskr/Task.swift
+++ Taskr/Task.swift
@@ -2,22 +2,21 @@
 //  Task.swift
 //  Taskr
 //
-//  Created by Alex Gerstein (student LM) on 2/21/20.
+//  Created by Maxwell Serota (student LM) on 2/25/20.
 //  Copyright © 2020 BigBrainCorporation. All rights reserved.
-// test
+//
 
 import Foundation
-
 class Task{
 
     var description = ""
     var person = ""
     var dueDate = Date()
-    
+
     init(description: String, person: String, dueDate: Date){
         self.description = description
         self.person = person
         self.dueDate = dueDate
     }
-    
+
 }
